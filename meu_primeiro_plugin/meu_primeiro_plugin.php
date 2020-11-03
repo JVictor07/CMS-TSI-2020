24c1c6b2d5e2d53a8649ef75f10613018128e69e
João Victor <joao.victor@huia.haus>
2020-11-02 22:35:59 -0300

    códigos do dia 27/10

--- meu_primeiro_plugin/meu_primeiro_plugin.php
+++ meu_primeiro_plugin/meu_primeiro_plugin.php
@@ -14,3 +14,24 @@ add_filter('login_errors', 'altera_msg_login');
 function altera_msg_login() {
   return 'Credenciais inválidas';
 }
+
+// wp_head -- Add things on <head>
+add_action('wp_head', 'colocar_og_tags');
+
+function colocar_og_tags() {
+  if ( is_single() ) {
+
+    $dados_do_post = get_post( get_the_ID() );
+
+    $titulo = $dados_do_post->post_title;
+
+    $resumo = $dados_do_post->post_excerpt;
+
+    echo "  \n\n\n
+            <meta property='og:title' content='" . $titulo . "'>\n
+            <meta property='og:site_name' content='" . get_bloginfo() . "'>\n
+            <meta property='og:url' content='" . get_permalink() . "'>\n
+            <meta property='og:description' content='" . $resumo . "'>\n
+            \n\n\n";
+  }
+}
